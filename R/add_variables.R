34d1654cb98a0205f81efe162cfdbca8b19e0e53
Gildas Glemarec <ggle@dtu.dk>
2025-01-21 10:05:16 +0100

    Adding the fix for missing soak times and the fix for distance to shore

--- R/add_variables.R
+++ R/add_variables.R
@@ -2,9 +2,13 @@
 #' Take in the Black Box data from the BBimport function and adds a number of important parameters
 #' @param x Data output from function BBimport
 #' @param give_me_more Should optional variables not relevant for RDBES be created too (default is TRUE)?
+#' @param path_to_soak Some soak time info is missing (not found in BB). Manual fix based on a prior extraction (input is a csv file)
+#' @param path.to.coastline ESRI Shapefile of the region (used to estimate distance to shore). By default, the file is Europe (Bounding box:  xmin: 943609.8 ymin: -375446 xmax: 7601958 ymax: 6825119; Projected CRS: ETRS89-extended / LAEA Europe)
 #' @return Same dataset with additional columns
 #' @export
-add_variables <- function(x = data_work, give_me_more = give_me_more) {
+add_variables <- function(x = data_work, give_me_more = give_me_more,
+                          path_to_soak = path_to_soak,
+                          path.to.coastline = path.to.coastline) {
   . <- data_work <- y <- m <- d <- quarter <- lat.start <- lat.stop <- lon.start <- lon.stop <- rnum <- NULL
   x$rnum <- as.integer(row.names(x))
   data.table::setDT(x,'rnum')
@@ -41,18 +45,41 @@ add_variables <- function(x = data_work, give_me_more = give_me_more) {
   ## Make sure dataframe is organized the way we want
   x <- dplyr::arrange(x, rnum)
 
+  ## Soak times
+  ### List the hauls with manually determined soak times
+  soak_files <- list.files(path_to_soak,
+                           pattern = "*csv",
+                           full.names = TRUE,
+                           recursive = FALSE)
+  list_soak <- lapply(soak_files,
+                      utils::read.csv2,
+                      ## Uncomment below if the format looks weird (and comment the line above)
+                      # utils::read.csv, sep = ",",
+                      header=TRUE,
+                      stringsAsFactors = FALSE,
+                      quote = "")
+  soakdata <- data.table::rbindlist(list_soak,fill=TRUE)
+  soakdata <- soakdata[, c('IDhaul','soak')]
+  soakdata$IDhaul <- as.factor(soakdata$IDhaul)
+  soakdata <- data.table::setkey(soakdata,IDhaul)
+  x <- data.table::setkey(x,IDhaul)
+  x <- soakdata[rem_data_from2021, on = .(IDhaul)]
+  x[is.na(soak), soak:=i.soak] # only replace the value missing from left table
+  x[,"i.soak":=NULL]
+
   ## Standardised effort
-  ## as net length (in km) multiplied by soak time (hours): km*hour
-  x$std_effort <- as.numeric(x$soak * x$netlength / 1000) # netlength is in meters
+  ### as net length (in km) multiplied by soak time (hours): km*hour
+  x$std_effort <- as.numeric(x$soak * x$netlength / 1000) # netlength is in m
 
   if (give_me_more == TRUE){
     ## Add info on depth (m) at point
     x <- ggleR::get.depth(x)
     ## Add info on distance (m) to nearest point on shore
-    coastline <- sf::st_read(
-      "Q:/gis/Dynamisk/GEOdata2020/BasicLayers/Coastlines/Europe/EEA Europe/EEA_Coastline_20170228.shp")
+    coastline <- sf::st_read(path.to.coastline
+      # "Q:/gis/Dynamisk/GEOdata2020/BasicLayers/Coastlines/Europe/EEA Europe/EEA_Coastline_20170228.shp"
+      )
     x_sf <- x  %>%
-sf::st_as_sf(coords = c('lon.haul','lat.haul'), na.fail = FALSE,
+      sf::st_as_sf(coords = c('lon.haul','lat.haul'), na.fail = FALSE,
                    crs = 4326) %>%
       sf::st_transform(3035)
     distances <- sapply(1:nrow(x_sf), function(i) {
