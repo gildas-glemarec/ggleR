11cd6a471e9d546150cfbd427b25f513afac16cf
Gildas Glemarec <ggle@dtu.dk>
2025-08-22 15:25:22 +0200

    Bug fix

--- R/fix.CQ.R
+++ R/fix.CQ.R
@@ -18,7 +18,7 @@ fix.CQ <- function(x = "Q:/10-forskningsprojekter/faste-cctv-monitoring/data/bla
     names(dat) <- strsplit(header, ';')[[1]] # add headers
     dat <- dat[-1,] ## Remove top row (duplicate of header)
     ## Species list #----
-    if( length(spp_list) == 0 ){
+    if( length(spp_list) != 0 ){
       list2env(spp_list, envir = .GlobalEnv)
     }else{
       print("You should load a species list explicitely. Here, the following species list is loaded per default. Check that it is what you need or update otherwise./n
